7518d6c644b33adfc56c06a7b44917a40c7f8c82
Chloe Lee <chloealessandra@gmail.com>
2016-07-22 11:30:02 -0400

    adds existing information into edit form fields to prepopulate form

--- resources/views/tasks/partials/_form.blade.php
+++ resources/views/tasks/partials/_form.blade.php
@@ -1,17 +1,17 @@
 <div class="form">
 	<div class="form-item">
 	    {!! Form::label('name', 'Name:', ['class'=>'field-label']) !!}
-	    {!! Form::text('name', '', ['class'=>'text-field']) !!}
+	    {!! Form::text('name', $task->name, ['class'=>'text-field']) !!}
 	</div>
 
 	<div class="form-item">
 	    {!! Form::label('slug', 'Slug:', ['class'=>'field-label']) !!}
-	    {!! Form::text('slug', '', ['class'=>'text-field']) !!}
+	    {!! Form::text('slug', $task->slug, ['class'=>'text-field']) !!}
 	</div>
 
 	<div class="form-item">
 	    {!! Form::label('description', 'Description:', ['class'=>'field-label']) !!}
-	    {!! Form::textarea('description', '', ['class'=>'text-field']) !!}
+	    {!! Form::textarea('description', $task->description, ['class'=>'text-field']) !!}
 	</div>
 
 	<div class="form-item">
--- resources/views/teams/partials/_form.blade.php
+++ resources/views/teams/partials/_form.blade.php
@@ -2,11 +2,11 @@
 	<div class="form">
 		<div class="form-item">
 		    {!! Form::label('name', 'Name:', ['class'=>'field-label']) !!}
-		    {!! Form::text('name', '', ['class'=>'text-field']) !!}
+		    {!! Form::text('name', $team->name, ['class'=>'text-field']) !!}
 		</div>
 		<div class="form-item">
 		    {!! Form::label('slug', 'Slug:', ['class'=>'field-label']) !!}
-		    {!! Form::text('slug', '', ['class'=>'text-field']) !!}
+		    {!! Form::text('slug', $team->slug, ['class'=>'text-field']) !!}
 		</div>
 		<div class="form-actions -padded">
 		    {!! Form::submit($submit_text, ['class'=>'button -secondary']) !!}

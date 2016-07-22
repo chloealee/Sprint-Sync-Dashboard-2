d9702f6747b2fe09bec81695064de966b682bff9
Chloe Lee <chloealessandra@gmail.com>
2016-07-22 10:43:51 -0400

    starts creating the team_members table

---
+++ database/migrations/2016_07_22_143340_create_team_members_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamMembersTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('team_members');
+    }
+}
